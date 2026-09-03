91d85f6e3d822fff0dd4c69e2df4dd37367638be
Asif Shah <asifshah9530608@gmail.com>
2026-09-03 13:51:41 +0500

    Added automated GitHub Actions deployment pipeline

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',       // Swapped from 'standalone' to optimize for static asset hosting
+  trailingSlash: true,    // Fixes 404 asset errors when users refresh internal sub-pages
 };
 
 module.exports = nextConfig;
